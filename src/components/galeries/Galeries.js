4aaadfceebc1f17291d36da75dbec3d41fa12f63
rezhariansyah <rezharatulangi@gmail.com>
2020-01-15 14:31:50 +0700

    galery update

--- src/components/galeries/Galeries.js
+++ src/components/galeries/Galeries.js
@@ -4,6 +4,10 @@ import { Fade } from "react-awesome-reveal";
 import image1 from "../../assets/images/galeries/IMG_0182.jpg";
 import image2 from "../../assets/images/galeries/IMG_0461.jpg";
 import image3 from "../../assets/images/galeries/IMG_3178.JPG";
+import image4 from "../../assets/images/categories/about.jpg";
+import image5 from "../../assets/images/categories/health.jpg";
+import image6 from "../../assets/images/categories/tips.jpg";
+import image7 from "../../assets/images/categories/travel.png";
 
 const images = [
   {
@@ -16,17 +20,35 @@ const images = [
     src: image2,
     thumbnail: image2,
     thumbnailWidth: 320,
-    thumbnailHeight: 212,
-    tags: [
-      { value: "Ocean", title: "Ocean" },
-      { value: "People", title: "People" }
-    ],
-    caption: "Boats (Jeshu John - designerspics.com)"
+    thumbnailHeight: 212
   },
-
   {
     src: image3,
     thumbnail: image3,
+    thumbnailWidth: 320,
+    thumbnailHeight: 212
+  },
+  {
+    src: image4,
+    thumbnail: image4,
+    thumbnailWidth: 320,
+    thumbnailHeight: 412
+  },
+  {
+    src: image5,
+    thumbnail: image5,
+    thumbnailWidth: 320,
+    thumbnailHeight: 212
+  },
+  {
+    src: image6,
+    thumbnail: image6,
+    thumbnailWidth: 320,
+    thumbnailHeight: 212
+  },
+  {
+    src: image7,
+    thumbnail: image7,
     thumbnailWidth: 320,
     thumbnailHeight: 212
   }
@@ -35,10 +57,10 @@ const images = [
 class Galeries extends Component {
   styleSmall() {
     return {
-        width: "100%",
-        backgroundSize: "cover",
-        objectFit: "cover",
-        cursor: "pointer"
+      width: "100%",
+      backgroundSize: "cover",
+      objectFit: "cover",
+      cursor: "pointer"
     };
   }
 
@@ -54,10 +76,10 @@ class Galeries extends Component {
             </div>
             <div className="row justify-content-center mx-auto">
               <div className="col-8 text-center">
-                <Gallery 
-                    images={images} 
-                    thumbnailStyle={this.styleSmall} 
-                    enableImageSelection={false}
+                <Gallery
+                  images={images}
+                  thumbnailStyle={this.styleSmall}
+                  enableImageSelection={false}
                 />
               </div>
             </div>
